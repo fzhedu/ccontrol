9214db21d2e4eb838dcad555cc6000643210956d
fzh <fzhedu@gmail.com>
2018-03-02 20:17:55 +0800

    update cache-test

--- cache-test/cache.cpp
+++ cache-test/cache.cpp
@@ -1,8 +1,18 @@
 #include<stdio.h>
-#include<ccontrol.h>
-void do_stuff(char *t, size_t s);
+extern "C"
+{
+#include "ccontrol.h"
+//void do_stuff(char *t, size_t s);
+}
+
+void print(char *t, size_t s) {
+	for(int i =0; i< s; ++i) {
+		printf("%c",t[i]);
+	}
+	puts("");
+}
 int main(void) {
-	char *t;
+	char *t=0;
 	struct ccontrol_zone *z;
 	color_set c;
 
@@ -14,8 +24,9 @@ int main(void) {
 	ccontrol_create_zone(z,&c,400);
 
 	t = (char *) ccontrol_malloc(z,100);
-	do_stuff(t,100);
-
+	printf("the address of memory is %d \n",t);
+	//do_stuff(t,100);
+	//print(t,10);
 	ccontrol_free(z,t);
 	ccontrol_destroy_zone(z);
 	ccontrol_delete(z);
